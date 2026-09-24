ace4e5fdd40f0e75d8703a7dd2da4b263fbd5e0a
Conor <38096133+Conor-Xia@users.noreply.github.com>
2026-09-24 21:43:49 +0800

    Optimize blog branding, analytics, and article rendering

--- conf/code.config.js
+++ conf/code.config.js
@@ -4,9 +4,9 @@
 module.exports = {
   // START********代码相关********
   // PrismJs 代码相关
-  PRISM_JS_PATH: 'https://npm.elemecdn.com/prismjs@1.29.0/components/',
+  PRISM_JS_PATH: 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/',
   PRISM_JS_AUTO_LOADER:
-    'https://npm.elemecdn.com/prismjs@1.29.0/plugins/autoloader/prism-autoloader.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/plugins/autoloader/prism-autoloader.min.js',
 
   // 代码主题 @see https://github.com/PrismJS/prism-themes
   PRISM_THEME_PREFIX_PATH:
